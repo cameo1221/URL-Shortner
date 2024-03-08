b5bdfa8eff5da22575e839dedb14c95cfaeedfb5
cameo1221 <78523086+cameo1221@users.noreply.github.com>
2024-03-08 13:29:36 +0530

    Handler func added

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/cameo1221/URL-Shortner
+
+go 1.22.0
+
+require github.com/go-yaml/yaml v2.1.0+incompatible // indirect
--- main.go
+++ main.go
@@ -1,1 +1,29 @@
-package main 
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/cameo1221/URL-Shortener/handler"
+)
+
+func main() {
+	mux := defaultMux()
+
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+	// yaml := `
+	// -path: /urlshort
+	// url: http://www.google.co.uk/
+	// -path: /urlshort-final
+	// url: https`
+	// yamlHandler. err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	// if err != nil{
+	// 	panic(err)
+	// }
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", mapHandler)
+
+}
